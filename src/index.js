be76f2920b7e5310a4ab438ee0a380758cdf9bab
Liam Chiang <jhc752314@gmail.com>
2021-12-19 00:48:27 +0800

    feat: update from twilio to mitake

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const {
     Helpers: { requireConfig },
 } = require('@genx/app');
 const { ExternalServiceError } = require('@genx/error');
+const superagent = require('superagent');
 
 /**
- * Twilio SMS feature
- * @module Feature_SmsTwilio
+ * Mitake SMS feature
+ * @module Feature_SmsMitake
  */
 
 module.exports = {
@@ -26,33 +27,103 @@ module.exports = {
      * Load the feature
      * @param {App} app - The app module object
      * @param {object} options - Options for the feature
-     * @property {string} options.accountSid - The ACCOUNT SID param.
-     * @property {string} options.authToken - The AUTH TOKEN param.
-     * @property {string} options.from - The from param, e.g. +14155552671
+     * @property {string} options.username - The USERNAME param.
+     * @property {string} options.password - The PASSWORD param.
+     * @property {string} options.domain - The DOMAIN param.
+     * @property {string} options.from - The FROM param.
      * @see {@link https://www.twilio.com/docs/glossary/what-e164|E.164}
      * @returns {Promise.<*>}
      *
      * @example
      *
-     * accountSid: '<ACCOUNT SID>',
-     * authToken: '<AUTH TOKEN>'
+     * username: '<USERNAME>',
+     * password: '<PASSWORD>'
+     * domain: '<DOMAIN>'
+     * from: '<FROM>'
      */
     load_: async function (app, options, name) {
-        requireConfig(app, options, ['accountSid', 'authToken', 'from'], name);
+        requireConfig(app, options, ['username', 'password', 'domain', 'from'], name);
 
-        const { accountSid, authToken, from } = options;
-        const Twilio = app.tryRequire('twilio');
+        const { username, password, domain, from } = options;
 
-        const client = new Twilio(accountSid, authToken);
+        const authQuery = { username, password };
+        const client = superagent;
 
         const service = {
             from,
-            sendSms_: async (to, body) => {
+            viewCredit_: async (number) => {
                 try {
-                    return await client.messages.create({ from, to, body });
+                    const SmQuery = new URL('/api/mtk/SmQuery', domain).href;
+                    const res = await client.post(SmQuery).query(authQuery).query(number);
+                    const results = res.text.split('\r\n');
+                    const statuscode = (results.find(res => res.includes('statuscode')))?.replace('statuscode=', '');
+                    const error = (results.find(res => res.includes('Error')))?.replace('Error=', '');
+                    const AccountPoint = (results.find(res => res.includes('AccountPoint')))?.replace('AccountPoint=', '');
+                    const info = {};
+                    if (AccountPoint) {
+                        info.dateCreated = { AccountPoint };
+                    }
+                    if (statuscode && error) {
+                        info.errorMessage = { statuscode, errorMessage: error };
+                    }
+
+                    return info;
                 } catch (error) {
                     throw new ExternalServiceError(
-                        'Failed to send sms via twilio.',
+                        'Failed to check credit point via mitake.',
+                        error
+                    );
+                }
+            },
+            smsSend_: async (to, body) => {
+                try {
+                    const sendMultiple = Array.isArray(to);
+                    const apiPath = sendMultiple ? '/api/mtk/SmBulkSend' : '/api/mtk/SmSend';
+                    const sendSmsUrl = new URL(apiPath, domain).href;
+                    let res;
+                    if (sendMultiple) {
+                        const parsedToList = to.map(user => {
+                            const { ClientID, dstaddr, dlvtime, vldtime, destname, response } = user;
+                            const parsedData = [ClientID, dstaddr, dlvtime, vldtime, destname, response, body].join('$$');
+                            return parsedData;
+                        });
+                        const encodingPostIn = { Encoding_PostIn: "UTF8" };
+                        const parsedBody = parsedToList.join("\r\n");
+                        res = await client.post(sendSmsUrl).query(authQuery).query(encodingPostIn).pipe(parsedBody);
+                        console.log('res: ', res);
+                    }
+                    else {
+                        const encodedBody = { smbody: body };
+                        const encodingPostIn = { CharsetURL: "UTF8" };
+                        res = await client.post(sendSmsUrl).query(authQuery).query(encodingPostIn).query(to).query(encodedBody);
+                    }
+                    
+                    const result = res?.text || '';
+                    const info = {};
+
+                    const userList = result.split(/\[[0-9]+\]/).filter(text => text!== '');
+                    const results = userList.map(user => {
+                        const userDetail = user.split("\r\n").filter(text => text!== '');
+                        const msgid = (userDetail.find(res => res.includes('msgid')))?.replace('msgid=', '');
+                        const statuscode = (userDetail.find(res => res.includes('statuscode')))?.replace('statuscode=', '');
+                        const AccountPoint = (userDetail.find(res => res.includes('AccountPoint')))?.replace('AccountPoint=', '');
+                        return {msgid, statuscode, AccountPoint}
+                    });
+
+                    const errorcode = (userList.find(res => res.includes('statuscode')))?.replace('statuscode=', '');
+                    const error = (userList.find(res => res.includes('Error')))?.replace('Error=', '');
+
+                    if (results.length > 0) {
+                        info.dateCreated = results;
+                    }
+                    if (errorcode && error) {
+                        info.errorMessage = { statuscode: errorcode, errorMessage: error };
+                    }
+
+                    return info;
+                } catch (error) {
+                    throw new ExternalServiceError(
+                        'Failed to send sms via mitake.',
                         error
                     );
                 }
--- test/smoke.spec.js
+++ test/smoke.spec.js
@@ -2,16 +2,59 @@ const testSuite = require("@genx/test");
 
 testSuite(
     __filename,
-    function (suite) {
-        suite.testCase("smoke test", async function () {                 
-            suite.startWorker_(async (app) => {
-                const twilio = app.getService('twilio');
-                should.exist(twilio);
-
-                const result = await twilio.sendSms_(null /** to be replaced by a verified number */, 'test from sandbox');
+    async (suite) => {
+        await suite.testCase("smoke test", async () => {                 
+            await suite.startWorker_(async (app) => {
+                const mitake = app.getService('mitake');
+                should.exist(mitake);
+                const result = await mitake.viewCredit_();
                 should.exist(result);
                 should.exist(result.dateCreated);
-                should.not.exist(result.errorMessage);
+                should.equal(result.errorMessage, undefined);
+            });
+        });
+        await suite.testCase("send single sms test", async () => {                 
+            await suite.startWorker_(async (app) => {
+                const body = "這是簡訊測試，this is sms testing.";
+                const singleSms = { 
+                    ClientID: null,
+                    dstaddr: '0900311212',
+                    dlvtime: null,
+                    vldtime: null,
+                    destname: null
+                };
+                const multipleSms = [
+                    { 
+                        ClientID: null,
+                        dstaddr: '0900311212',
+                        dlvtime: null,
+                        vldtime: null,
+                        destname: null
+                    },
+                    { 
+                        ClientID: null,
+                        dstaddr: '0900311212',
+                        dlvtime: null,
+                        vldtime: null,
+                        destname: null
+                    }
+                ];
+
+                const mitake = app.getService('mitake');
+                should.exist(mitake);
+
+                // const singleResult = await mitake.smsSend_(singleSms, body);
+                // console.log('singleResult: ', singleResult);
+                // should.exist(singleResult);
+                // should.exist(singleResult.dateCreated);
+                // should.equal(singleResult.errorMessage, undefined)
+
+                const multiResult = await mitake.smsSend_(multipleSms, body);
+                console.log('multiResult: ', multiResult);
+                should.exist(multiResult);
+                should.exist(multiResult.dateCreated);
+                should.
+                should.equal(multiResult.errorMessage, undefined)
             });
         });
     }, 
